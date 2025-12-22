853ef6439100c127b227a5bf5322b9f39dc35084
husniddin <husniddin@debian.debian>
2025-12-22 08:21:44 +0500

    create mobile menu

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\UserContactController;
 use App\Http\Controllers\Web\UserController;
 use App\Http\Controllers\Web\UserDataController;
@@ -18,10 +17,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::resources([
         'users' => UserController::class,
         'workers' => WorkerController::class,
