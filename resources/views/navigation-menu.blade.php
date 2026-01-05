e863d6c998978b6c8f11c86042911648c3af1265
husniddin <husniddin@debian.debian>
2026-01-05 16:42:06 +0500

    create ad icon

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -53,12 +53,15 @@
                     <span class="text-lg font-semibold text-gray-800">
                         {{ $balanse }}
                     </span>
-                    <div class="text-blue-500">
+                    <div class="relative text-blue-500">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
                         </svg>
+                        <span
+                            class="absolute -top-2 -right-2 bg-red-500 text-white text-xs 
+                 rounded-full px-1.5 py-0.5">3</span>
                     </div>
                 </div>
                 <!-- Settings Dropdown -->
@@ -97,6 +100,8 @@
                                 {{ __('Profile') }}
                             </x-dropdown-link>
 
+                            
+
                             <div class="border-t border-gray-200"></div>
 
                             <!-- Authentication -->
@@ -127,12 +132,15 @@
                     <span class="text-lg font-semibold text-gray-800">
                         {{ $balanse }}
                     </span>
-                    <div class="text-blue-500">
+                    <div class="relative text-blue-500">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
                         </svg>
+                        <span
+                            class="absolute -top-2 -right-2 bg-red-500 text-white text-xs 
+                 rounded-full px-1.5 py-0.5">3</span>
                     </div>
                 </div>
                 <button @click="open = ! open"
